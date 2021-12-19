adda23c998e468fb9cd36d41c2367bd84e5311d1
shira samson <shira77192@gmail.com>
2021-12-19 17:25:01 +0200

    Add project files.

---
+++ DAL/Models/FavoriteCitys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Models
+{
+    public partial class FavoriteCitys
+    {
+        public int Id { get; set; }
+        public string IdUser { get; set; }
+        public string IdName { get; set; }
+        public string CityName { get; set; }
+        public string WeatherText { get; set; }
+        public int Temp { get; set; }
+    }
+}
---
+++ serverWeather/Controllers/FavoriteCitysController.cs
@@ -0,0 +1,49 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+namespace serverWeather.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FavoriteCitysController : ControllerBase
+    {
+        private readonly IFavoriteCitysBL favoriteCitysBL ;
+        // GET: <controller>
+        public FavoriteCitysController(IFavoriteCitysBL _favoriteCitysBL)
+
+        {
+            this.favoriteCitysBL = _favoriteCitysBL ;
+        }
+
+        [HttpGet("Get")]
+        public ActionResult <List<FavoriteCitys>> Get(string id)
+        {
+            return favoriteCitysBL.GetCitysById(id);
+        }
+
+        [HttpPost]
+        [Route("AddCity")]
+        public void AddCity(FavoriteCitys favoriteCitys)
+        {
+
+            favoriteCitysBL.AddCity(favoriteCitys);
+
+        }
+        [HttpDelete("DeleteCityById")]
+        public void DeleteCityById(string id )
+        {
+            var idUser = id.Split("@")[0];
+            var idName = id.Split("@")[1];
+
+            favoriteCitysBL.DeleteCityById(idUser, idName);
+
+        }
+
+
+
+    }
+}
